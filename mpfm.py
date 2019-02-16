a51a70e7dadd729278341ae9b7caeb9fac2073f1
michael <michael.catchen@colorado.edu>
2019-02-16 12:41:11 -0700

    ok

--- mpfm.py
+++ mpfm.py
@@ -15,12 +15,11 @@ def main():
     print_info()
 
     if (args['BATCH']):
-        area_decay_space = [0.1, 0.5, 2.0]
-        dist_decay_space = [0.5, 1.0, 2.0]
-        total_indiv_space = [4000, 6000, 8000]
-        n_patches_space = [5, 10, 25]
+        dist_decay_space = [1.0]
+        n_ef_space = [1, 2, 3, 4]
+        n_patches_space = [50]
         ef_space = [0.2, 0.5, 0.8]
-        lw_space = [0.05, 0.2, 0.4]
+        lw_space = [0.05, 0.2]
 
         n_rep = 30
 
@@ -29,43 +28,44 @@ def main():
         print('Num of Procs: %d' % n_threads)
         #pool = ThreadPool(n_threads)
 
-
+        lpef = [0, 20, 10, 7, 5]
 
         treatment_ct = 0
 
-        for area in area_decay_space:
-            for dist_decay in dist_decay_space:
-                for n_patches in n_patches_space:
-                    for n_indiv in total_indiv_space:
-                        for ef_h in ef_space:
-                            for lw in lw_space:
-                                treatment_ct += 1
-                                path = 'treatment%d_rep' % (treatment_ct)
+        for dist_decay in dist_decay_space:
+            for n_patches in n_patches_space:
+                for n_ef in num_ef_space:
+                    for ef_h in ef_space:
+                        for lw in lw_space:
+                            treatment_ct += 1
+                            path = 'treatment%d_rep' % (treatment_ct)
 
-                                for rep in range(n_rep):
-                                    os.chdir(this_dir)
-                                    params = {}
-                                    for param in param_table:
-                                        params[param] = param_table[param]['default']
+                            for rep in range(n_rep):
+                                os.chdir(this_dir)
+                                params = {}
+                                for param in param_table:
+                                    params[param] = param_table[param]['default']
 
 
-                                    params["BASE_MIGRATION_RATE"] = 0.05
+                                params["BASE_MIGRATION_RATE"] = 0.05
 
-                                    params["MEAN_LOCUS_WEIGHT"] = lw
-                                    params["PATCH_DECAY"] = area
-                                    params["N_INDIVIDUALS"] = n_indiv
-                                    params["INCIDENCE_FUNCTION_DECAY"] = dist_decay
-                                    params["NUM_PATCHES"] = n_patches
-                                    params["ENV_FACTOR_H_VALUE"] = ef_h
+                                params["MEAN_LOCUS_WEIGHT"] = lw
+                                params["PATCH_DECAY"] = 1.0
+                                params["N_INDIVIDUALS"] = 8000
+                                params["INCIDENCE_FUNCTION_DECAY"] = dist_decay
+                                params["NUM_PATCHES"] = n_patches
+                                params["ENV_FACTOR_H_VALUE"] = ef_h
+                                params["NUM_ENV_FACTORS"] = n_ef
+                                params["NUM_LOCI_PER_EF"] = lpef[n_ef]
 
-                                    params["DATA_DIRECTORY"] = path + str(rep)
-                                    params["RANDOM_SEED"] = np.random.randint(0, 100000000)
-                                    params["EF_RANDOM_SEED"] = np.random.randint(0, 100000000)
-                                    params["PATCH_RANDOM_SEED"] = np.random.randint(0, 100000000)
-                                    params["GENOME_RANDOM_SEED"] = np.random.randint(0, 100000000)
+                                params["DATA_DIRECTORY"] = path + str(rep)
+                                params["RANDOM_SEED"] = np.random.randint(0, 100000000)
+                                params["EF_RANDOM_SEED"] = np.random.randint(0, 100000000)
+                                params["PATCH_RANDOM_SEED"] = np.random.randint(0, 100000000)
+                                params["GENOME_RANDOM_SEED"] = np.random.randint(0, 100000000)
 
-                                    # Each treatment goes into unique lb_cmd_file
-                                    create_batch_run_file(this_dir, params, treatment_ct)
+                                # Each treatment goes into unique lb_cmd_file
+                                create_batch_run_file(this_dir, params, treatment_ct)
 
     else:
         params = {}
